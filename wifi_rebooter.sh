47ee133d8756fc3bed6cdd80b4558fa0a4f71341
Paul Adams <pauladams509@gmail.com>
2016-08-14 09:14:58 -0700

    --wip--

--- wifi_rebooter.sh
+++ wifi_rebooter.sh
@@ -11,6 +11,6 @@ ping -c2 ${SERVER} > /dev/null
 if [ $? != 0 ]
 then
     # Restart the wireless interface
-    ifdown --force wlan0
-    ifup wlan0
+    sudo ip link set down wlan0
+    sudo ip link set up wlan0
 fi
